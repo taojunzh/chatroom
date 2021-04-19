15dedc60cb46c245ac23385454cd69f74f5aa3e0
zzzzcyt <46452789+zzzzcyt@users.noreply.github.com>
2021-04-19 13:17:30 -0400

    ytc_inprogress

--- test_mongo.py
+++ test_mongo.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify, render_template,redirect,url_for,request,session
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO,join_room,leave_room
+from flask_login import current_user, login_user, login_required, logout_user, LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 import uuid
 import pymongo
 #from passlib.hash import pbkdf2_sha256
-myclient = pymongo.MongoClient('mongo')
+# myclient = pymongo.MongoClient('mongo')
+myclient = pymongo.MongoClient("mongodb+srv://ytc:kevin@cluster0.35txz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = myclient.user_login_system
 #chat_history_database.drop()
+
 accounts_collection = db["accounts"]
 
 
@@ -16,19 +19,33 @@ app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-mydb= SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
 Online_Users=[]
 
 
 class User:
 
+    # def __init__(self,display,username,password):
+    #     self.username =username
+    #     self.display = display
+    #     self.password = password
+    #
+    # @staticmethod
+    # def is_authenticated():
+    #     return True
+    #
+    # @staticmethod
+    # def is_active():
+    #     return True
+
     def start_session(self, user):
         del user['password']
         session['logged_in'] = True
         session['user'] = user
-        #Online_Users.append(user["display"].title())
-        Online_Users.append("d2")
-        print(user["display"].title())
+        Online_Users.append(user["display"].title())
+        # Online_Users.append("d2")
         return
 
     def signup(self):
@@ -44,7 +61,7 @@ class User:
         #user['password'] = pbkdf2_sha256.encrypt(user['password'])
         if db.users.insert_one(user):
             self.start_session(user)
-            #return redirect(url_for('login'))
+            return redirect(url_for('login'))
         else:
             return render_template('register.html', error="register failed.")
 
@@ -58,13 +75,19 @@ class User:
         #if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
         if user and request.form.get('password') == user['password']:
             self.start_session(user)
-            return redirect(url_for('chatroom'))
+            return redirect(url_for('index'))
 
         return render_template('login.html', error="Invalid username or password. Please try again.")
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/chatroom')
 def chatroom():
@@ -90,12 +113,24 @@ def login():
 @socketio.on('connection established')
 def connect():
     print("connection established")
-    new_onlineuser =session.get('display')
-    print(new_onlineuser)
-    socketio.emit('connection received',(new_onlineuser))
+    new_onlineuser =session.get('user')
+    new_onlineuser = new_onlineuser['display']
+    print(session)
+    print(Online_Users)
+    socketio.emit('connection received',(new_onlineuser,Online_Users))
+
+# @socketio.on('disconnect')
+# def disconnect():
+#     dis_user= session.get('user')
+#     Online_Users.remove(dis_user['display'])
+#
+#     socketio.emit('disconnectd',dis_user['display'])
+@login_manager.user_loader
+def load_user(user_id):
+    return User.get(user_id)
 
 if __name__ == '__main__':
 
     #socketio.run(app)
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True) #use this line when using docker
-    #app.run(port=5000, debug=True)
+    # socketio.run(app, host="0.0.0.0", port=5000, debug=True) #use this line when using docker
+    app.run(port=5000, debug=True)
