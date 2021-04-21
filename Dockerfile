a92c1a10bb636f5cea7c3e048022eb8e8833c614
NoCode <36163454+taojunzh@users.noreply.github.com>
2021-04-20 20:35:40 -0400

    authentication

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ WORKDIR /root
 
 COPY . .
 RUN python -m pip install Flask flask-sqlalchemy flask-socketio flask_login
-RUN python -m pip install pymongo requests
+RUN python -m pip install pymongo requests bcrypt
 
 EXPOSE 5000
 
--- test_mongo.py
+++ test_mongo.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, render_template,redirect,url_for,request,session
-from flask_socketio import SocketIO,join_room,leave_room
+from flask import Flask, jsonify, render_template, redirect, url_for, request, session
+from flask_socketio import SocketIO, join_room, leave_room
 from flask_login import current_user, login_user, login_required, logout_user, LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,10 +7,11 @@ import requests
 import datetime
 import uuid
 import pymongo
+import bcrypt
 #from passlib.hash import pbkdf2_sha256
 myclient = pymongo.MongoClient("mongodb://mongo:27017")
 db = myclient.user_login_system
-#chat_history_database.drop()
+# chat_history_database.drop()
 
 accounts_collection = db["accounts"]
 
@@ -23,7 +24,7 @@ socketio = SocketIO(app)
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
-Online_Users=[]
+Online_Users = []
 
 
 class User:
@@ -50,13 +51,23 @@ class User:
         return
 
     def signup(self):
-        print(request.form)
+        # print(request.form)
+        password = request.form.get('password').encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password, salt)
         user = {
             "_id": uuid.uuid4().hex,
             "display": request.form.get('display'),
             "user": request.form.get('username'),
-            "password": request.form.get('password')
+            "password": hashed,
+            "password_salt": salt
         }
+        # user = {
+        #     "_id": uuid.uuid4().hex,
+        #     "display": request.form.get('display'),
+        #     "user": request.form.get('username'),
+        #     "password": request.form.get('password')
+        # }
 
         # Encrypt the password
         #user['password'] = pbkdf2_sha256.encrypt(user['password'])
@@ -72,17 +83,26 @@ class User:
         user = db.users.find_one({
             "user": request.form.get('username')
         })
+        password = request.form.get('password').encode("utf-8")
 
-        #if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
-        if user and request.form.get('password') == user['password']:
+        salt = user["password_salt"]
+        hashed = bcrypt.hashpw(password, salt)
+        # if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+        if user and hashed == user['password']:
             self.start_session(user)
             return redirect(url_for('index'))
 
         return render_template('login.html', error="Invalid username or password. Please try again.")
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    print(session.get("user")['display'])
+    if session.get("user")['display']:
+        return render_template('index.html', status="you have logged in")
+    else:
+        return render_template('index.html', status="Please Login first")
+
 
 @app.route("/logout/")
 @login_required
@@ -90,15 +110,21 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
 @app.route('/chatroom')
 def chatroom():
-    return render_template('chatroom.html')
+    if len(Online_Users) > 0:
+        return render_template('chatroom.html')
+    else:
+        return redirect(url_for('index'))
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == 'POST':
         return User().signup()
     return render_template('register.html')
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -111,14 +137,14 @@ def login():
 # def chatroom():
 #     return render_template('chatroom.html')
 
+
 @socketio.on('connection established')
 def connect():
     print("connection established")
-    new_onlineuser =session.get('user')
+    new_onlineuser = session.get('user')
     new_onlineuser = new_onlineuser['display']
-    print(session)
-    print(Online_Users)
-    socketio.emit('connection received',(new_onlineuser,Online_Users))
+
+    socketio.emit('connection received', (new_onlineuser, Online_Users))
 
 # @socketio.on('disconnect')
 # def disconnect():
@@ -128,44 +154,50 @@ def connect():
 #     socketio.emit('disconnectd',dis_user['display'])
 @socketio.on("message")
 def message(data):
-    if(data["type"]=="comment"):
+    if(data["type"] == "comment"):
         socketio.send({
-        'username': data["username"],
-        'comment': HTMLescape(data["comment"]),
-        "type": data["type"],
-        'time': datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            'username': data["username"],
+            'comment': HTMLescape(data["comment"]),
+            "type": data["type"],
+            'time': datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
         })
-    if(data["type"]=="link"):
-        embedded= data["link"].replace("https://www.youtube.com/watch?v=","https://www.youtube.com/embed/")
+    if(data["type"] == "link"):
+        embedded = data["link"].replace(
+            "https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/")
         if "&ab_channel=" in embedded:
-            embedded= embedded.split("&ab_channel=")
-            embedded= embedded[0]
+            embedded = embedded.split("&ab_channel=")
+            embedded = embedded[0]
 
         socketio.send({
-        'username': data["username"],
-        'link': HTMLescape(data["link"]),
-        'embedded': HTMLescape(embedded),
-        'valid': check_url(data["link"]),
-        "type": data["type"],
-        'time': datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+            'username': data["username"],
+            'link': HTMLescape(data["link"]),
+            'embedded': HTMLescape(embedded),
+            'valid': check_url(data["link"]),
+            "type": data["type"],
+            'time': datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
         })
+
 
 def HTMLescape(string):
-    return string.replace("&","&amp").replace("<","&lt").replace(">","&gt")
+    return string.replace("&", "&amp").replace("<", "&lt").replace(">", "&gt")
+
 
 def check_url(url):
     try:
         response = requests.head(url)
-        return 1 if (200==response.status_code && "youtube.com" in url )else 0
+        return 1 if (200 == response.status_code and "youtube.com" in url)else 0
     except:
         return 0
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.get(user_id)
 
+
 if __name__ == '__main__':
 
-    #socketio.run(app)
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True) #use this line when using docker
+    # socketio.run(app)
+    # use this line when using docker
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
     # app.run(port=5000, debug=True)
